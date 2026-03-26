6c06805c719d5392798771d1eaac52485a85baad
Farhan-Mr <farhat.hp2022@gmail.com>
2026-03-26 21:30:19 +0530

    Table with range

---
+++ Clg/tableType2.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class tableType2
+{
+    public static void main(String [] args)
+    {
+        number obj1 = new number();
+        obj1.Display();
+    }
+}
+
+class number
+{
+    int range;
+    void Display()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter range -> ");
+        range = sc.nextInt();
+
+            for(int i=1; i<=range; i++)
+            {
+                for(int j=1; j<=10; j++)
+                {
+                    System.out.println(i + " * " + j + " = " + (j*i));
+                }
+                System.out.println("\n");
+            }
+    }
+}
