5c6b8102425926d12c4181bb810e5cf857966022
Farhan-Mr <farhat.hp2022@gmail.com>
2026-03-26 14:40:23 +0530

    Hello print Day 1

---
+++ Clg/hello.java
@@ -0,0 +1,17 @@
+class hey
+{
+    void display()
+    {
+        System.out.println("Hello");
+    } 
+    
+}
+
+public class hello 
+{
+    public static void main(String [] args)
+    {
+        hey h = new hey();
+        h.display();
+    }
+}
