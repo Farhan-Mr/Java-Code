964a21b2ccd025a50dac809510517bc70c886e33
Farhan-Mr <farhat.hp2022@gmail.com>
2026-03-27 16:04:29 +0530

    Check Eligibility

---
+++ Clg/ageCondition.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class ageCondition 
+{
+    public static void main(String [] args)
+    {
+        age obj = new age();
+        obj.input();
+        obj.display();
+    }    
+}
+
+class age
+{
+    int a;
+    void input()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your age : ");
+        a = sc.nextInt();
+    }
+    void display()
+    {
+        if (a >= 18) 
+            {
+                System.out.println("Elegible for Voting and Driving ");
+            }
+            else{
+                System.out.println("Not Elegible ");
+            }
+    }
+}
